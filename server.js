f48b778a91ac8ea7ace5f38bb82d8d5a0f735327
makedasp <makedaphoenix@gmail.com>
2021-11-26 17:18:49 -0500

    update

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get("/app/user/:id", (req, res) => {
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user, md5(req.body.id));
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.params.id);
 	res.status(200).json({"message": + stmt.changes + "record updated: ID " + stmt.lastInsertRowid + " (200)"});
 });
 
