e6b0e449c4c14099cd5423d765c0da19f3ac9903
makedasp <makedaphoenix@gmail.com>
2021-11-25 23:27:08 -0500

    a04_1.1

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
+var db = require("./database.js");
+var md5 = require("md5");
 
 // Require md5 MODULE
 
@@ -10,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
+var HTTP_PORT = 5000;
 
 // Start server
 app.listen(HTTP_PORT, () => {
@@ -23,6 +26,9 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new", (req, rest) =>{
+	const stmt = db.prepare("INSERT INTO userinfo(user, pass) VALUES (?, ?)").run();
+})
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
@@ -31,6 +37,10 @@ app.get("/app/users", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/users/:id", (req, res) => {
+	const stmt = db.prepare("SELECT * FROM userinfo").get();
+	res.status(200).json(stmt);
+})
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 
@@ -38,6 +48,6 @@ app.get("/app/users", (req, res) => {
 
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
